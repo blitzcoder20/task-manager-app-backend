45d26fb2bdf04da19d3018121ba324783d00864f
blitzcoder20 <raffaele.serra00@outlook.com>
2024-03-30 01:08:51 +0100

    added database manager class and created a connection test on server.ts

---
+++ src/dbManager.ts
@@ -0,0 +1,44 @@
+import { Pool, PoolClient, QueryArrayResult } from 'pg';
+import dotenv from 'dotenv';
+
+class dbManager {
+
+    private pool: Pool;
+
+    private client: PoolClient | undefined
+
+    constructor(){
+
+        //setup env variables
+        dotenv.config();
+
+        //initializes a db connection pool
+        this.pool  = new Pool({
+            database: process.env.DB_DATABASE,
+            host: process.env.DB_HOST,
+            password: process.env.DB_PASSWORD,
+            port: parseInt(process.env.DB_PORT ?? "5432"),
+            user: process.env.DB_USER,
+        });
+
+        this.client = undefined;
+    }
+
+    async createConnection() {
+        if (!this.pool) {
+            throw new Error('Database pool is not initialized');
+        } 
+        //connect to the database
+        this.client = await this.pool.connect();
+    }
+
+    async query(query:string):Promise<QueryArrayResult>{
+        if(!this.client){
+            throw new Error('You must connect to the db first')
+        }
+        return await this.client.query(query);
+    }
+    
+}
+
+export default dbManager;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express,{Request,Response} from "express"
+import { Pool } from 'pg';
+import dbManager from "./dbManager";
 
 const app = express();
 
-app.get('/',(req:Request,res:Response)=>{
-    res.send("Hello world!");
+app.get('/',async (req:Request,res:Response)=>{
+    const db = new dbManager();
+    db.createConnection().then(()=>{
+        db.query("Select * from users").then(result => console.log(result.rows));
+    })
+
 });
 
 app.listen(3000, () => {
